4db10c45e3e78eb327d2b98337e38288271197b5
Sophia <spuertas@micds.org>
2018-06-21 20:32:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ function nestedTarget() {
   return document.querySelector('.target');
 }
 function increaseRankBy(n) {
-  
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+ 
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toString()
+  }
 }
