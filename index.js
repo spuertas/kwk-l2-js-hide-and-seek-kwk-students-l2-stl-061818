15a4a90cb0c40470753041b4ed996a3c9962ca61
Sophia <spuertas@micds.org>
2018-06-21 21:18:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ function deepestChild() {
   return document.querySelector('#grand-node div div div div');
 }
 function increaseRankBy(n) {
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
- 
-  for (let i = 0; i < lis.length; i++) {
-    var a = parseInt(i);
-    lis[a].innerHTML = (a + n).toString();
+const rank = document.querySelectorAll('ul.ranked-list');
+
+for (let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = (i + 1).toString();
   }
 }
