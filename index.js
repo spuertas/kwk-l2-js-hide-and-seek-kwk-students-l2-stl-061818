2c7606222c79d14e03ecd4844a30c75481acc3ba
Sophia <spuertas@micds.org>
2018-06-21 20:33:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function increaseRankBy(n) {
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
  
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (i + n).toString()
+    lis[i].innerHTML = (i + n+1).toString()
   }
 }
