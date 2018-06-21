512575d702c09f5fb9b638f4d01a02d5f24dbe1e
Sophia <spuertas@micds.org>
2018-06-21 21:18:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Code your solution here
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.querySelector('.target');
+}
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
+function increaseRankBy(n) {
+const rank = document.querySelectorAll('ul.ranked-list');
+
+for (let i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = (i + 1).toString();
+  }
+}
