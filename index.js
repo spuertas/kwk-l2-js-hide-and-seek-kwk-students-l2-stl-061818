36dc66238e1b3a8549f7bbae1ac33ec6226e9cc8
Sophia <spuertas@micds.org>
2018-06-21 20:36:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function nestedTarget() {
 function deepestChild() {
 }
 function increaseRankBy(n) {
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
  
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (i + n).toString()
+    lis[i].innerHTML = (i + n).toString();
   }
 }
